b14d2c9a60638731b1309a985edfd090b12fd39e
Miguel florian <migflor04@gmail.com>
2021-04-11 00:21:37 +0100

    commit on master

--- connexion.php
+++ connexion.php
@@ -4,18 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="connexion.css">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="./css/connexion.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="back">
+        <a href="#"><i class="las la-arrow-circle-left la-2x" ></i>Revenir à l'accueil</a>
+    </div>
     <div class="form">
         <h2>Veuillez bien renseigner vos accès !</h2>
         <form action=""method="">
             <div class="input-group">
-                <input type="email" name="email" id="email" placeholder="Enter votre adresse email">
+                <i class="las la-envelope la-2x"></i><input type="email" name="email" id="email" placeholder="Enter votre adresse email">
             </div>
             <div class="input-group">
-                <input type="password" name="password" id="password" placeholder="Entrer votre mot de passe">
+                <i class="las la-key la-2x"></i><input type="password" name="password" id="password" placeholder="Entrer votre mot de passe">
             </div>
             <div class="input-group">
                 <input type="submit" value="Se connecter">
