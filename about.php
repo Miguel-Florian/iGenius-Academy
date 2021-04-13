f40125beecff5bec161c7a80a8cf0d56b5ebcbd0
Miguel florian <migflor04@gmail.com>
2021-04-13 22:22:30 +0100

    commit on master

---
+++ about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/about.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <?php include"./includes/nav.php"; ?>
+    </header>
+    <div class="container">
+        <aside>
+            <ul>
+                <li><a href="#">Administration</a></li>
+                <li><a href="#">Différentes facultés</a></li>
+                <li><a href="#">Cerémonies et Prix</a></li>
+                <li><a href="#">Visite guidée</a></li>
+            </ul>
+        </aside>
+        <div class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur blanditiis, ea explicabo quis consequuntur iure nemo, labore harum laboriosam atque totam! Sed voluptatibus quasi ea ipsum sit ut nostrum reiciendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, maiores labore. Eveniet ea voluptatum aspernatur. Voluptate, vel nisi ipsam, nostrum sapiente explicabo doloremque tenetur illo quidem, numquam ullam recusandae laudantium. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptatem saepe quaerat sapiente! Ipsam qui, asperiores, deserunt eaque illo tempore eius libero debitis sed nemo atque iusto autem? Minima, molestias! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil aperiam atque totam nulla earum, fuga temporibus. Veritatis, cupiditate dignissimos rerum fugiat alias illo nobis eaque magnam laudantium, nihil provident non?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit dicta veritatis autem nisi unde, nihil quo officia, ea ab esse neque nostrum porro at doloribus amet praesentium optio quibusdam nesciunt?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam, mollitia aspernatur nisi quidem hic unde molestiae magnam quam eligendi aliquid tenetur placeat cum magni enim ad debitis saepe necessitatibus animi</div>
+    </div>
+</body>
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -2,10 +2,10 @@
             <div class="logo"><img src="./images/4926342.png" alt=""></div>
                 <div class="nav-link">
                     <ul>
-                        <li class="link"><a href="#">Accueil</a></li>
-                        <li class="link"><a href="#">iGenius</a></li>
-                        <li class="link"><a href="#">Connexion</a></li>
-                        <li class="link"><a href="#">Contact</a></li>
+                        <li class="link"><a href="../index.php">Accueil</a></li>
+                        <li class="link"><a href="../about.php">iGenius</a></li>
+                        <li class="link"><a href="../connexion.php">Connexion</a></li>
+                        <li class="link"><a href="../contact.php">Contact</a></li>
                     </ul>
                 </div>
 </nav>
