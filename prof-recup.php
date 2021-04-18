4889994d4c00bee135f342594ff5cb67df86a4e4
Miguel florian <migflor04@gmail.com>
2021-04-18 02:53:28 +0100

    commit on master

---
+++ prof-recup.php
@@ -0,0 +1,27 @@
+<?php
+   $query="SELECT * FROM professeurs WHERE email='{$mail}' && matricule ='{$pass}' ";
+   $select_user_query=mysqli_query($connexion,$query);
+   if(!($select_user_query)){
+       die("<b>Query Failed</b>".mysqli_error($connexion));
+   }
+   while($row=mysqli_fetch_array($select_user_query)){
+       //echo($row['id']." ".$row['Nom']." ".$row['Prenom']." ".$row['Email']);
+        //var_dump($row);
+        $db_nom=$row["Nom"];
+        $db_prenom=$row["Prenom"];
+        $db_email=$row["Email"];
+        $db_password=$row["Matricule"];
+    }
+        if($mail!==$db_email && $pass!==$db_password){
+            header("Location:error.php");
+        }else if($mail==$db_email && $pass==$db_password){
+
+            $_SESSION['nom']=$db_nom;
+            $_SESSION['prenom']=$db_prenom;
+            $_SESSION['email']=$db_email;
+            header("Location:prof.php");
+        }else{
+             header("Location:index.php");
+        }
+        
+?>
--- recup_donne.php
+++ recup_donne.php
@@ -4,30 +4,15 @@ session_start();
 if(isset($_POST['submit'])){
    $mail=htmlentities(trim($_POST['email']));
    $pass=htmlentities(trim($_POST['password']));
-   $query="SELECT * FROM etudiants WHERE email='{$mail}' && matricule ='{$pass}' ";
-   $select_user_query=mysqli_query($connexion,$query);
-   if(!($select_user_query)){
-       die("<b>Query Failed</b>".mysqli_error($connexion));
+   
+   if($_POST['statut']=="etudiant"){
+       include "./eleve-recup.php";
    }
-   while($row=mysqli_fetch_array($select_user_query)){
-       //echo($row['id']." ".$row['Nom']." ".$row['Prenom']." ".$row['Email']);
-        //var_dump($row);
-        $db_nom=$row["Nom"];
-        $db_prenom=$row["Prenom"];
-        $db_email=$row["Email"];
-        $db_password=$row["Matricule"];
-    }
-        if($mail!==$db_email && $pass!==$db_password){
-            header("Location:error.php");
-        }else if($mail==$db_email && $pass==$db_password){
-
-            $_SESSION['nom']=$db_nom;
-            $_SESSION['prenom']=$db_prenom;
-            $_SESSION['email']=$db_email;
-            header("Location:etudiant.php");
-        }else{
-             header("Location:prof.php");
-        }
+   else if ($_POST['statut']=="professeur"){
+       include "./prof-recup.php";
+   }else{
+       header("Location:index.php");
+   }
   
 }
 ?>
