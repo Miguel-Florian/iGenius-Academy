2c8b9d9c4c8a8bb17f67ea4eb8bd4f0286a762f0
Miguel florian <migflor04@gmail.com>
2021-04-18 13:01:53 +0100

    commit on master

--- etudiant.php
+++ etudiant.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/etudiant.css">
     <?php $titre="Etudiant";?>
     <title>Dashboard | <?=$titre;?></title>
 </head>
 <body>
-    <p>je suis etudiant</p>
+    
 </body>
 </html>
