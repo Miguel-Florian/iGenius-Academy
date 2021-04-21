73f2818199dc4db58ecce0bc64d461542a7fed19
Miguel florian <migflor04@gmail.com>
2021-04-21 20:47:07 +0100

    commit on master

--- etudiant.php
+++ etudiant.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/etudiant.css">
+    <scrip src="index.js"></scrip>
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
     <?php $titre="Etudiant";?>
     <title>Dashboard | <?=$titre;?></title>
@@ -13,24 +14,24 @@
     <div class="sidebar">
         <div class="sidebar-brand"><h1><span class="lab la-accusoft"></span>iGenius</h1></div>
         <div class="sidebar-menu">
-            <ul>
-                <li>
-                    <a href="" class="active"><span class="las la-igloo"></span><span>Dashboard</span></a>
+            <ul class="container-onglets">
+                <!--<li class="onglets active">
+                    <a href="" class="onglets active" data-anim="1"><span class="las la-igloo"></span><span>Dashboard</span></a>
+                 </li>-->
+                 <li>
+                    <a href=""class="onglets active"data-anim="1"><span class="lar la-clipboard"></span><span>Consulter les notes</span></a>
                  </li>
                  <li>
-                    <a href=""><span class="lar la-clipboard"></span><span>Consulter les notes</span></a>
+                    <a href=""class="onglets"data-anim="2"><span class="las la-book-open"></span><span>Consulter les cours</span></a>
+                 </li >
+                 <li>
+                    <a href=""class="onglets"data-anim="3"><span class="las la-file-invoice"></span><span>Ecrire une requete</span></a>
                  </li>
                  <li>
-                    <a href=""><span class="las la-book-open"></span><span>Consulter les cours</span></a>
-                 </li>
-                 <li>
-                    <a href=""><span class="las la-file-invoice"></span><span>Ecrire une requete</span></a>
-                 </li>
-                 <li>
-                    <a href=""><span class="lab la-wpforms"></span><span>Inscription académique</span></a>
+                    <a href=""class="onglets"data-anim="4"><span class="lab la-wpforms"></span><span>Inscription académique</span></a>
                  </li> 
                  <li>
-                    <a href=""><span class="las la-cog"></span><span>Settings</span></a>
+                    <a href=""class="onglets"data-anim="5"><span class="las la-cog"></span><span>Settings</span></a>
                  </li>  
             </ul>
         </div>
@@ -92,8 +93,52 @@
                     </div>
                 </div>
             </div>
+            <div class="contenu activeContenu" data-anim="1">
+                <h2>Conculter les notes</h2>
+            </div>
+            <div class="contenu" data-anim="2">
+                <h2>Conculter les cours</h2>
+            </div>
+            <div class="contenu" data-anim="3">
+                <h2>Ecrire une requete</h2>
+            </div>
+            <div class="contenu" data-anim="4">
+                <h2>Inscription academique</h2>
+            </div>
+            <div class="contenu" data-anim="5">
+                <h2>Settings</h2>
+            </div>
 
         </main>
     </div>
+    <script>
+        const onglets = document.querySelectorAll(".onglets");
+        const contenu = document.querySelectorAll(".contenu");
+        let index = 0;
+
+onglets.forEach((onglet) => {
+  onglet.addEventListener("focus", () => {
+    if (onglet.classList.contains('active')) {
+      return;
+    } else {
+      onglet.classList.add('active');
+    }
+    index = onglet.getAttribute('data-anim');
+    console.log(index);
+    for (i = 0; i < onglets.length; i++) { 
+      if (onglets[i].getAttribute('data-anim') != index) {
+        onglets[i].classList.remove('active');
+      }
+    }
+    for(j=0;j<contenu.length;j++){
+      if(contenu[j].getAttribute('data-anim')==index){
+        contenu[j].classList.add('activeContenu'); 
+      }else{
+        contenu[j].classList.remove('activeContenu');
+      }
+    }
+  });
+});
+    </script>
 </body>
 </html>
