08a71d778b2f96aa732186c8a6400f8bce064bb4
Miguel florian <migflor04@gmail.com>
2021-04-28 17:45:29 +0100

    update includes

--- includes/nav.php
+++ includes/nav.php
@@ -2,10 +2,10 @@
             <div class="logo"><img src="./images/iGenius-100.png" alt=""></div>
                 <div class="nav-link">
                     <ul>
-                        <li class="link active"id="index"><a href= "index.php">Accueil</a></li>
-                        <li class="link"id="about"data-anim="2"><a href="about.php">iGenuis</a></li>
-                        <li class="link"id="connexion"data-anim="3"><a href="connexion.php">Connexion</a></li>
-                        <li class="link"id="contact"data-anim="4"><a href="contact.php">Contact</a></li>
+                        <li class="link <?php if($_SERVER['SCRIPT_NAME']==='/iGenius Academy/index.php'):?>active <?php endif;?>"id="index"><a href= "index.php">Accueil</a></li>
+                        <li class="link <?php if($_SERVER['SCRIPT_NAME']==='/iGenius Academy/about.php'):?>active <?php endif;?>"id="about"data-anim="2"><a href="about.php">iGenuis</a></li>
+                        <li class="link <?php if($_SERVER['SCRIPT_NAME']==='/iGenius Academy/connexion.php'):?>active <?php endif;?>"id="connexion"data-anim="3"><a href="connexion.php">Connexion</a></li>
+                        <li class="link <?php if($_SERVER['SCRIPT_NAME']==='/iGenius Academy/contact.php'):?>active <?php endif;?>"id="contact"data-anim="4"><a href="contact.php">Contact</a></li>
                     </ul>
                 </div>
 </nav>
