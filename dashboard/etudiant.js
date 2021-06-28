00d9ebbdda1c09db6441da589bdcba66d3e7a899
“Miguel-Florian” <migflor04@gmail.com>
2021-06-28 19:03:40 +0100

    update etudiant.js

--- dashboard/etudiant.js
+++ dashboard/etudiant.js
@@ -4,3 +4,11 @@ btn.onclick = function () {
   nav.classList.toggle("nav_open");
 };
 
+function toggleMenu(){
+        let toggle=document.querySelector('.toggle');
+        let nav=document.querySelector('.nav');
+        let main=document.querySelector('.main');
+        toggle.classList.toggle('active');
+        nav.classList.toggle('active');
+        main.classList.toggle('active');
+    }
