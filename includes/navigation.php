54ba617fc5582e5d25d95edeb99c321d9f2c2fd7
“Miguel-Florian” <migflor04@gmail.com>
2021-06-29 15:32:02 +0100

    create navigation for dashboard

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,56 +1,56 @@
 <div class="nav">
         <ul>
             <li>
-                <a href="#">
-                    <span class="icon"></span>
-                    <span class="title"><h2>Brand Name</h2></span>
+                <a href="../index.php">
+                    <span class="icon"><img src="../images/iGenius-50.png" alt=""></span>
+                    <span class="title"><h2>iGenius Academy</h2></span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="fas fa-home"></i></span>
+                <a href="#" id="dashboard">
+                    <span class="icon"><i class="las la-home"></i></span>
                     <span class="title">Dashboard</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="fas fa-tasks"></i></span>
+                <a href="#" id="requete">
+                    <span class="icon"><i class="las la-tasks"></i></span>
                     <span class="title">Requetes</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="far fa-file-pdf"></i></span>
+                <a href="#" id="notes">
+                    <span class="icon"><i class="las la-clipboard"></i></span>
                     <span class="title">Notes</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="far fa-book-open"></i></span>
+                <a href="#" id="cours">
+                    <span class="icon"><i class="las la-file-pdf"></i></span>
                     <span class="title">Cours</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="far fa-prescription-bottle"></i></span>
+                <a href="#" id="inscription">
+                    <span class="icon"><i class="lab la-wpforms"></i></span>
                     <span class="title">Inscription</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="far fa-prescription-bottle"></i></span>
+                <a href="#" id="message">
+                    <span class="icon"><i class="las la-comment"></i></span>
                     <span class="title">Communiqué</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="fal fa-question-circle"></i></span>
+                <a href="#" id="help">
+                    <span class="icon"><i class="las la-question-circle"></i></span>
                     <span class="title">Help</span>
                 </a>
             </li>
             <li>
-                <a href="#">
-                    <span class="icon"><i class="fas fa-lock"></i></span>
+                <a href="#" id="lock">
+                    <span class="icon"><i class="las la-lock"></i></span>
                     <span class="title">Déconnexion</span>
                 </a>
             </li>
