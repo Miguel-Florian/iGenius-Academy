b5845af62ce82c92e35353765032f880891b133c
“Miguel-Florian” <migflor04@gmail.com>
2021-06-29 15:33:57 +0100

    create userinfo layout

---
+++ includes/UserInfo.php
@@ -0,0 +1,18 @@
+<div class="details-etudiant">
+            <div class="title-info"><h3>Détails sur l'étudiant</h3></div>
+            <div class="row-information">
+                <div class="nom">Nom : <span>Noumowe Ngueukam</span></div>
+            </div>
+            <div class="row-information">
+                <div class="nom">Prénom: <span>Miguel Florian</span></div>
+            </div>
+            <div class="row-information">
+                <div class="nom">Niveau: <span>Licence 3</span></div>
+            </div>
+            <div class="row-information">
+                <div class="nom">Filière: <span>Informatique</span></div>
+            </div>
+            <div class="row-information">
+                <div class="nom">Sexe: <span>Masculin</span></h3></div>
+            </div>
+        </div>
