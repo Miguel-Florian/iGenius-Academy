da11b752891411c5862ab957ef0d03bd25db83b6
“Miguel-Florian” <migflor04@gmail.com>
2021-06-29 15:31:00 +0100

    create cardbox for student panel

---
+++ includes/CardBox.php
@@ -0,0 +1,38 @@
+<div class="cardBox">
+        <div class="card">
+            <div>
+                <div class="numbers">100</div>
+                <div class="cardName">Etudiants</div>
+            </div>
+            <div class="iconBox">
+                <i class="las la-users"></i>
+            </div>
+        </div>
+        <div class="card">
+            <div>
+                <div class="numbers">3</div>
+                <div class="cardName">Niveau</div>
+            </div>
+            <div class="iconBox">
+                <i class="las la-shoe-prints"></i>
+            </div>
+        </div>
+        <div class="card">
+            <div>
+                <div class="numbers">20</div>
+                <div class="cardName">En ligne</div>
+            </div>
+            <div class="iconBox">
+                <i class="las la-globe"></i>
+            </div>
+        </div>
+        <div class="card">
+            <div>
+                <div class="numbers">INF</div>
+                <div class="cardName">Parcours-type</div>
+            </div>
+            <div class="iconBox">
+                <i class="las la-university"></i>
+            </div>
+        </div>
+    </div>
