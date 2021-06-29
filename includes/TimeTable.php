95026aac45f0982bd354024d038460f387efb80f
“Miguel-Florian” <migflor04@gmail.com>
2021-06-29 15:33:02 +0100

    create timetable layout for student

---
+++ includes/TimeTable.php
@@ -0,0 +1,34 @@
+<div class="timetable">
+            <div class="title-info"><h3>Empoi de temps</h3></div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Jour</td>
+                        <td>Heure(debut-fin)</td>
+                        <td>Matières</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="color">
+                        <td>Lundi</td>
+                        <td>07h-16h</td>
+                        <td>UI/UX Design, Web Design</td>
+                    </tr>
+                    <tr>
+                        <td>Mercredi</td>
+                        <td>12h-18h</td>
+                        <td>Javascript, Python, Java</td>
+                    </tr>
+                    <tr class="color">
+                        <td>Jeudi</td>
+                        <td>08h-19h</td>
+                        <td>ReactJS, Django, Php</td>
+                    </tr>
+                    <tr>
+                        <td>Samedi</td>
+                        <td>07h-17h</td>
+                        <td>ReactJS, Django, Php</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
