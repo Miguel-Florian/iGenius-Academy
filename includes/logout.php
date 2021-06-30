3c859f9677b95da9a45b6ae0bcb4616ba9b9b036
“Miguel-Florian” <migflor04@gmail.com>
2021-06-30 12:45:42 +0100

    create logout.php

---
+++ includes/logout.php
@@ -0,0 +1,13 @@
+<div class="card-off">
+    <div class="alert">
+        <i class="las la-exclamation-triangle la-4x"></i>
+        <h4>Souhaitez-vous vous déconnecter ?</h4>
+    </div>
+    <div class="deep-message">
+        <p>En vous déconnectant, vous perdrez tout le travail aboutit jusqu'ici si celui ci n'a pas été sauvegardé !</p>
+    </div>
+    <div class="button-response">
+        <button class="response">Oui</button>
+        <button class="response">Non</button>
+    </div>
+</div>
