01e7d891add9f3d7672dc0e06c18d50ab70aa49d
“Miguel-Florian” <migflor04@gmail.com>
2021-07-04 17:32:36 +0100

    update etudiant.php

--- dashboard/etudiant.php
+++ dashboard/etudiant.php
@@ -24,51 +24,85 @@ setcookie('user',$_SESSION['prenom']);
 <div class="main">
     <?php include "../includes/TopBar.php"?>
     <?php include "../includes/CardBox.php"?>
-    <div class="informations"></div>
+    <div class="informations"><?php include "../includes/content.php"?></div>
 
 </div>
 
 <script src="./etudiant.js"></script>
 <script>
+let link = document.querySelectorAll("li a[href='#']");
+  console.log(link)
     $("#dashboard").click(function(e){
         $(".informations").load("../includes/content.php");
         let dash = document.querySelector("#dashboard");
-        dash.classList.add("active")
+        if(dash.classList.contains("active")){
+            dash.classList.remove("active")
+        }else{
+            dash.classList.add("active")
+        }
     })
     $("#requete").click(function(e){
         $(".informations").load("../includes/requete.php");
         let req = document.querySelector("#requete");
-        req.classList.add("active")
+        if(req.classList.contains("active")){
+            req.classList.remove("active")
+        }else{
+            req.classList.add("active")
+        }
     })
     $("#notes").click(function(e){
         $(".informations").load("../includes/note.php");
         let note = document.querySelector("#notes");
-        note.classList.add("active")
+        if(note.classList.contains("active")){
+            note.classList.remove("active")
+        }else{
+            note.classList.add("active")
+        }
     })
     $("#cours").click(function(e){
         $(".informations").load("../includes/cour.php");
         let cour = document.querySelector("#cours");
-        cour.classList.add("active")
+        if(cour.classList.contains("active")){
+            cour.classList.remove("active")
+        }else{
+            cour.classList.add("active")
+        }
     })
     $("#inscription").click(function(e){
         $(".informations").load("../includes/ins-aca.php");
         let ins = document.querySelector("#inscription");
-        ins.classList.add("active")
+        if(ins.classList.contains("active")){
+            ins.classList.remove("active")
+        }else{
+            ins.classList.add("active")
+        }
     })
     $("#message").click(function(e){
         $(".informations").load("../includes/communique.php");
         let message = document.querySelector("#message");
-        message.classList.add("active")
+        if(message.classList.contains("active")){
+            message.classList.remove("active")
+        }else{
+            message.classList.add("active")
+        }
     })
     $("#help").click(function(e){
         $(".informations").load("../includes/help.php");
         let help = document.querySelector("#help");
-        help.classList.add("active")
+        if(help.classList.contains("active")){
+            help.classList.remove("active")
+        }else{
+            help.classList.add("active")
+        }
     })
     $("#lock").click(function(e){
         $(".informations").load("../includes/logout.php");
         let lock = document.querySelector("#lock");
-        lock.classList.add("active")
+        if(lock.classList.contains("active")){
+            lock.classList.remove("active")
+        }else{
+            lock.classList.add("active")
+        }
     })
    
 </script>
