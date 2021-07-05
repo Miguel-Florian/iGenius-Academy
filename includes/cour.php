dc78f5e3930ff15cdc1bb100229c3c1d9d80f2bd
“Miguel-Florian” <migflor04@gmail.com>
2021-07-05 14:02:49 +0100

    modify cour.php

--- includes/cour.php
+++ includes/cour.php
@@ -1,1 +1,26 @@
-<h2>Conculter les cours</h2>
+<div class="card-help">
+    <div class="help-title"><i class="las la-file-pdf la-3x"></i><h3>Cours disponibles</h3></div>
+        <div class="card-item">
+            <div class="item">
+                <i class="las la-file-pdf la-2x"></i>
+                <div class="cour"><b>Introduction à Python</b></div>
+            </div>
+            <div class="item">
+                <i class="las la-file-pdf la-2x"></i>
+                <div class="cour"><b>Developper une Web App avec ReactJS</b></div>
+            </div>
+            <div class="item">
+                <i class="las la-file-pdf la-2x"></i>
+                <div class="cour"><b>Developper une Web App avec Vuesjs</b></div>
+            </div>
+            <div class="item">
+                <i class="las la-file-pdf la-2x"></i>
+                <div class="cour"><b>Developper un  site web avec PHP et Django</b></div>
+            </div>
+            <div class="item">
+                <i class="las la-file-pdf la-2x"></i>
+                <div class="cour"><b>Developper une application mobile avec JAVA</b></div>
+            </div>
+        </div>        
+    
+</div>
