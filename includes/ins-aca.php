4a8fff9e194ec28df8ab23d68e4ef8509498a8b9
“Miguel-Florian” <migflor04@gmail.com>
2021-07-05 13:18:29 +0100

    updating

--- includes/ins-aca.php
+++ includes/ins-aca.php
@@ -1,22 +1,74 @@
-<h2>Inscription academique</h2>
-                <div class="signup">
-                    <div class="signup-droit">
-                        <form action="etu-recup" method="post">
-                            <div class="matricule"><label for="matricule">Matricule</label><input type="text" name="matricule" id="matricule"></div>
-                            <div class="filiere"><label for="filiere">Filiere</label>
-                                <select name="filiere" id="filiere">
-                                    <option value="informatique">Informatique</option>
-                                    <option value="mathematique">Mathematique</option>
-                                    <option value="physique">Physique</option>
-                                </select>
-                            </div>
-                            <div class="semestre"><label for="semestre">semestre</label>
-                                <select name="semestre" id="semestre">
-                                    <option value="semestre">Semestre 1</option>
-                                    <option value="semestre">Semestre 2</option>
-                                </select>
-                            </div>
-                            <div class="send"><input type="submit" value="Envoyer"></div>
-                        </form>
-                    </div>
-                </div>
+<div class="card-sign">
+    <div class="signup">
+        <h3>Inscription Academique</h3>
+        <div class="input-group">
+            <label for="level">Filière : </label>
+                <select>
+                    <option value="soft">Software Engeneering</option>
+                    <option value="marketing">Database</option>
+                    <option value="design">Design 2D / 3D</option>
+                    <option value="ui">UX/UI Design</option>
+                </select>
+        </div>
+        <div class="input-group">
+            <label for="level">Niveau : </label>
+                <select>
+                    <option value="level3">Niveau 3</option>
+                    <option value="level2">Niveau 2</option>
+                    <option value="level1">Niveau 1</option>
+                </select>
+        </div>
+        <div class="input-group">
+            <label for="level">Semestre : </label>
+                <select>
+                    <option value="semestre1">Semestre 1</option>
+                    <option value="semestre2">Semestre 2</option>
+                </select>
+        </div>
+        <div class="input-group">
+                <input type="submit" name="submit" value="Envoyer">
+        </div>
+    </div>
+    <div class="element">
+        <table class="table-element">
+                <thead>
+                    <tr>
+                        <td><h4>Code</h4></td>
+                        <td><h4>Intitulé</h4></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="color">
+                        <td><input type="checkbox" value="pyhton"> Soft306</td>
+                        <td>Pyhon</td>
+                        
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" value="react"> Soft316</td>
+                        <td>ReactJS</td>
+                        
+                    </tr>
+                    <tr class="color">
+                        <td><input type="checkbox" value="vue"> Soft326</td>
+                        <td>VueJS</td>
+                        
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox"value="django"> Soft336</td>
+                        <td>Django</td>
+                        
+                    </tr>
+                    <tr class="color">
+                        <td><input type="checkbox" value="java"> Soft346</td>
+                        <td>Java</td>
+                        
+                    </tr>
+                    <tr>
+                        <td><input type="checkbox" value="php"> Soft356</td>
+                        <td>Php</td>
+                        
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+</div>
