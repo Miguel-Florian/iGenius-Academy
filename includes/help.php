0da9135a704155d887038cdeec989405f4966cc9
“Miguel-Florian” <migflor04@gmail.com>
2021-07-05 13:18:05 +0100

    updating

--- includes/help.php
+++ includes/help.php
@@ -1,3 +1,29 @@
-<div class="card-off">
+<div class="card-help">
+    <div class="help-title"><i class="las la-question-circle la-3x"></i><h3>Help</h3></div>
+        <div class="card-item">
+            <div class="item">
+                <i class="las la-question-circle la-3x"></i>
+                <div class="info">Pour la soumission de vos requetes, rassurez-vous
+                d'avoir composer cette matière. Au cas écheant, votre note sera raménée à 00/20
+                Et si par ailleurs, votre requete n'aboutit pas, contactez le professeur de la matière
+                en question.</div>
+            </div>
+            <div class="item">
+                <i class="las la-question-circle la-3x"></i>
+                <div class="info">Concernant les cours facultatifs et ou obligatoire, veuillez faire
+                une inscription académique dans le panel dashboard, afin que ce cour soit prisen compte
+                </div>
+            </div>
+            <div class="item">
+                <i class="las la-question-circle la-3x"></i>
+                <div class="info">Pour tous vos soucis d'inscription académique, veuillez vous rapprochez
+                au près de la cellule informatique</div>
+            </div>
+            <div class="item">
+                <i class="las la-question-circle la-3x"></i>
+                <div class="info">Pour vos soucis de connexion à la plateforme, ou d'usurpation d'identité
+                ou de votre compte veuillez vous rapprochez au plus vite près de la cellule informatique afin de palier à ce problème</div>
+            </div>
+        </div>        
     
 </div>
