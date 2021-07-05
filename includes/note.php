67411538114dc137d5e3db5a3311770a2518adb9
“Miguel-Florian” <migflor04@gmail.com>
2021-07-05 14:03:00 +0100

    modify note.php

--- includes/note.php
+++ includes/note.php
@@ -1,1 +1,7 @@
-<h2>Conculter les notes</h2>
+<div class="card-help">
+    <div class="help-title"><i class="las la-clipboard la-2x"></i><h3>Notes disponibles</h3></div>
+        <div class="card-item">
+        
+        </div>        
+    
+</div>
